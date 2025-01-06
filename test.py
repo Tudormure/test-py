832348f0456377ca495fd51d98dfdee65715e0fc
Tudormure <68165517+Tudormure@users.noreply.github.com>
2025-01-06 17:49:03 +0200

    test.py

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 import os
-import django==1.11.28
-import flask==0.12.3
-import requests==2.18.4
-import pillow==6.2.0
-import numpy==1.16.1
-import pandas==0.25.3
-import jinja2==2.10.1
-import pyyaml==4.2b1
-import urllib3==1.24.2
-import paramiko==2.6.0
-import cryptography==2.6.1
-import simplejson==3.16.0
-import matplotlib==3.0.3
-import twisted==19.2.0
-import sqlalchemy==1.3.10
+import django
+import flask
+import requests
+import pillow
+import numpy
+import pandas
+import jinja2
+import pyyaml
+import urllib3
+import paramiko
+import cryptography
+import simplejson
+import matplotlib
+import twisted
+import sqlalchemy
 def bandit_test(filename):
     os.system(f"{filename}")
 password = "adadadadaera"
